2983af59381fbd085eb22ea4551a40a5ba6afdce
Dustin Nation <dustin.nation@leasequery.com>
2023-07-01 21:42:18 -0400

    Add project files.

---
+++ StatePattern.GumballMachine/GumballMachine.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.GumballMachine
+{
+    public class GumballMachine
+    {
+        public GumballMachineStates State { get; set; }
+        public int Count { get; set; }
+
+        public GumballMachine(int count)
+        {
+            Count = count;
+
+            State = Count > 0
+                ? GumballMachineStates.NoQuarter
+                : GumballMachineStates.SoldOut;
+        }
+
+        public void InsertQuarter()
+        {
+            switch (State)
+            {
+                case GumballMachineStates.HasQuarter:
+                    Console.WriteLine("You can't insert another quarter");
+                    break;
+                case GumballMachineStates.NoQuarter:
+                    Console.WriteLine("You inserted a quarter");
+                    State = GumballMachineStates.HasQuarter;
+                    break;
+                case GumballMachineStates.SoldOut:
+                    Console.WriteLine("You can't insert a quarter, the machine is sold out!");
+                    break;
+                case GumballMachineStates.Sold:
+                    Console.WriteLine("Please wait, we're already dispensing your gumball");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void EjectQuarter()
+        {
+            switch (State)
+            {
+                case GumballMachineStates.HasQuarter:
+                    Console.WriteLine("Quarter returned");
+                    State = GumballMachineStates.NoQuarter;
+                    break;
+                case GumballMachineStates.NoQuarter:
+                    Console.WriteLine("You haven't inserted a quarter");
+                    break;
+                case GumballMachineStates.Sold:
+                    Console.WriteLine("Sorry, you already turned the crank");
+                    break;
+                case GumballMachineStates.SoldOut:
+                    Console.WriteLine("You can't eject, you haven't inserted a quarter yet");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void TurnCrank()
+        {
+            switch (State)
+            {
+                case GumballMachineStates.Sold:
+                    Console.WriteLine("Turning twice doesn't get you another gumball!");
+                    break;
+                case GumballMachineStates.NoQuarter:
+                    Console.WriteLine("You turned the crank, but you didn't insert a quarter");
+                    break;
+                case GumballMachineStates.SoldOut:
+                    Console.WriteLine("You turned, but there's no gumballs left to dispense");
+                    break;
+                case GumballMachineStates.HasQuarter:
+                    Console.WriteLine("You turned...");
+                    State = GumballMachineStates.Sold;
+
+                    Dispense();
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Dispense()
+        {
+            switch (State)
+            {
+                case GumballMachineStates.Sold:
+                    Console.WriteLine("A gumball comes rolling out of the slot");
+                    Count -= 1;
+                    
+                    if (Count == 0)
+                    {
+                        Console.WriteLine("Oops, out of gumballs");
+                        State = GumballMachineStates.SoldOut;
+                    }
+                    else
+                    {
+                        State = GumballMachineStates.NoQuarter;
+                    }
+
+                    break;
+                case GumballMachineStates.NoQuarter:
+                    Console.WriteLine("You need to insert a quarter first");
+                    break;
+                case GumballMachineStates.SoldOut:
+                    Console.WriteLine("No gumball dispensed");
+                    break;
+                case GumballMachineStates.HasQuarter:
+                    Console.WriteLine("You need to turn the crank");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ StatePattern.GumballMachine/Program.cs
@@ -0,0 +1,72 @@
+﻿using StatePattern.GumballMachine;
+
+var gumballMachine = new GumballMachine(5);
+
+PrintMachineState(gumballMachine);
+
+Console.WriteLine();
+
+// Happy path:
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+PrintMachineState(gumballMachine);
+
+Console.WriteLine();
+
+// Attempt to cheat the machine with no quarter:
+gumballMachine.InsertQuarter();
+gumballMachine.EjectQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+// Happy path:
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+// Happy path:
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+// Attempt to cheat the machine by getting a free quarter:
+gumballMachine.EjectQuarter();
+
+Console.WriteLine();
+
+PrintMachineState(gumballMachine);
+
+Console.WriteLine();
+
+// Attempt to insert a second quarter:
+gumballMachine.InsertQuarter();
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+// Happy path, but the machine should be empty now:
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+// Attempt to buy a gumball from an empty machine:
+gumballMachine.InsertQuarter();
+gumballMachine.TurnCrank();
+
+Console.WriteLine();
+
+PrintMachineState(gumballMachine);
+
+void PrintMachineState(GumballMachine gumballMachine)
+{
+    Console.WriteLine("Machine Status:  " + gumballMachine.State);
+    Console.WriteLine("Inventory:  " + gumballMachine.Count);
+}
